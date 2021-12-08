d49815bfc7c8a96197d4e1b3d98244726c2c448a
redundant4u <rafch@naver.com>
2021-12-08 22:17:46 +0900

    Test S2 Button interrupt

--- user/handler.c
+++ user/handler.c
@@ -1,88 +1,90 @@
 #include "nemo.h"
+#include "util.h"
 
 extern int stateScreen;
 
 void EXTI2_IRQHandler(void)
 {
     // Joystic DOWN
-    switch(stateScreen)
+    switch (stateScreen)
     {
-        case SCR_MAIN:
-            joystickMainScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
-            break;
-        case SCR_PAGE:
-            joystickStageScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
-            break;
-        case SCR_GAME:
-          joystickGameScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
-            break;
+    case SCR_MAIN:
+        joystickMainScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
+        break;
+    case SCR_PAGE:
+        joystickStageScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
+        break;
+    case SCR_GAME:
+        joystickGameScreen(EXTI_Line2, GPIOC, GPIO_Pin_2);
+        break;
     }
 }
 
 void EXTI3_IRQHandler(void)
 {
     // Joystic LEFT
-    switch(stateScreen)
+    switch (stateScreen)
     {
-        case SCR_MAIN:
-            joystickMainScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
-            break;
-        case SCR_PAGE:
-            joystickStageScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
-            break;
-        case SCR_GAME:
-          joystickGameScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
-            break;
+    case SCR_MAIN:
+        joystickMainScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
+        break;
+    case SCR_PAGE:
+        joystickStageScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
+        break;
+    case SCR_GAME:
+        joystickGameScreen(EXTI_Line3, GPIOC, GPIO_Pin_3);
+        break;
     }
 }
 
 void EXTI4_IRQHandler(void)
 {
     // Joystic RIGHT
-    switch(stateScreen)
+    switch (stateScreen)
     {
-        case SCR_MAIN:
-            joystickMainScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
-            break;
-        case SCR_PAGE:
-            joystickStageScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
-            break;
-        case SCR_GAME:
-          joystickGameScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
-            break;
+    case SCR_MAIN:
+        joystickMainScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
+        break;
+    case SCR_PAGE:
+        joystickStageScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
+        break;
+    case SCR_GAME:
+        joystickGameScreen(EXTI_Line4, GPIOC, GPIO_Pin_4);
+        break;
     }
 }
 
 void EXTI9_5_IRQHandler(void)
 {
     // Joystic UP
-    switch(stateScreen)
+    switch (stateScreen)
     {
-        case SCR_MAIN:
-            joystickMainScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
-            break;
-        case SCR_PAGE:
-            joystickStageScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
-            break;
-        case SCR_GAME:
-            joystickGameScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
-            break;
+    case SCR_MAIN:
+        joystickMainScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
+        break;
+    case SCR_PAGE:
+        joystickStageScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
+        break;
+    case SCR_GAME:
+        joystickGameScreen(EXTI_Line5, GPIOC, GPIO_Pin_5);
+        break;
     }
 }
 
 void EXTI15_10_IRQHandler(void)
 {
-    switch(stateScreen)
+    switch (stateScreen)
     {
-        case SCR_MAIN:
-            selectMainScreen(EXTI_Line11, GPIOD, GPIO_Pin_11);
-            break;
-        case SCR_PAGE:
-          selectStageScreen(EXTI_Line11, GPIOD, GPIO_Pin_11);
-            break;
-        case SCR_GAME:
-            
-            break;
+    case SCR_MAIN:
+        selectMainScreen(EXTI_Line11, GPIOD, GPIO_Pin_11);
+        backScreen(EXTI_Line12, GPIOD, GPIO_Pin_12);
+        break;
+    case SCR_PAGE:
+        selectStageScreen(EXTI_Line11, GPIOD, GPIO_Pin_11);
+        backScreen(EXTI_Line12, GPIOD, GPIO_Pin_12);
+        break;
+    case SCR_GAME:
+        break;
     }
 }
 
@@ -92,12 +94,12 @@ extern uint32_t Sound;
 // beeper handler
 void TIM2_IRQHandler(void)
 {
-    if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
     {
         TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
         Sound++;
 
-        if(Music <= Sound)
+        if (Music <= Sound)
         {
             GPIOB->ODR ^= GPIO_Pin_0;
             Sound = 0;
---
+++ user/util.h
@@ -0,0 +1,2 @@
+void delay(int ms);
+void backScreen(uint32_t EXTI_Line, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
